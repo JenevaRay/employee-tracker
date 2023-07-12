1c92ab76a9c73d2384ad955f11d8e754bccdf3a8
Jeneva Ray <jenevaisit@gmail.com>
2023-07-12 09:21:06 -0600

    added functional sql for inserts

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,5 +1,6 @@
 const inq = require('inquirer')
 const mysql = require('mysql2/promise')
+const {printTable} = require('console-table-printer')
 require('dotenv').config()
 
 let dbconn = null
@@ -25,24 +26,24 @@ async function doDBConn() {
 async function askTask() {
     async function selectDepartments() {
         let departments = await dbconn.query(
-            `SELECT name, id 
+            `SELECT name, id as department_id 
             FROM employee_db.department
-            ORDER BY id`
+            ORDER BY id;`
         )
-        
-        console.table(departments[0])
+        printTable(departments[0])
     }
     async function selectRoles() {
         let roles = await dbconn.query(
-            `SELECT title, role.id AS role_id, name AS department, salary
+            `SELECT title AS job_title, role.id AS role_id, name AS department, salary
             FROM employee_db.department 
-            INNER JOIN employee_db.role ON employee_db.role.department_id = employee_db.department.id;`
+            INNER JOIN employee_db.role ON employee_db.role.department_id = employee_db.department.id
+            ORDER BY role_id;`
         )
-        console.table(roles[0])
+        printTable(roles[0])
     }
     async function selectEmployees() {
         let employees = await dbconn.query(
-            `SELECT e.id AS employee_id, e.first_name, e.last_name, title, 
+            `SELECT e.id AS employee_id, e.first_name, e.last_name, title AS job_title, 
             name as department, salary, 
             CONCAT(m.first_name, ' ', m.last_name) AS manager
             FROM employee_db.department
@@ -50,7 +51,7 @@ async function askTask() {
             INNER JOIN employee_db.employee e ON e.role_id = employee_db.role.id
             INNER JOIN employee_db.employee m ON e.manager_id = m.id;`
         )
-        console.table(employees[0])
+        printTable(employees[0])
     }
     async function insertDepartment(name) {
         if (name.length > 0) {
@@ -61,7 +62,8 @@ async function askTask() {
                 throw result
             } else {
                 console.log(`Successfully added '${name}' department.`)
-                // console.log(result)
+                let inserted = await dbconn.query(`SELECT * FROM employee_db.department WHERE id = ?`, result[0].insertId)
+                printTable(inserted[0])
             }
         }
     }
@@ -73,7 +75,8 @@ async function askTask() {
             throw result
         } else {
             console.log(`Successfully added '${title}' role.`)
-            // console.log(result)
+            let inserted = await dbconn.query(`SELECT * FROM employee_db.role WHERE id = ?`, result[0].insertId)
+            printTable(inserted[0])
         }
     }
     async function addDepartment() {
@@ -115,6 +118,53 @@ async function askTask() {
                 // console.log(obj)
         })
     }
+    async function addEmployee() {
+        let title, salary, department_id
+        // we are getting the results of the whole departments table because we'll use it multiple times in multipel ways (caching)
+        let departments = await dbconn.query(
+            `SELECT name 
+            FROM employee_db.department
+            ORDER BY id`
+        )
+        let roles = await dbconn.query(
+            `SELECT title, 
+            FROM employee_db.role
+            ORDER BY id`
+        )
+        let managers = await dbconn.query(
+            `SELECT e.id AS employee_id, e.first_name, e.last_name, title AS job_title, 
+            name as department, salary, 
+            CONCAT(e.first_name, ' ', e.last_name) AS manager
+            FROM employee_db.department
+            INNER JOIN employee_db.role ON employee_db.role.department_id = employee_db.department.id
+            INNER JOIN employee_db.employee e ON e.role_id = employee_db.role.id
+            ORDER BY employee_id;`
+        )
+        console.log(managers)
+        await inq.prompt([
+            {name: "first_name", message: "Enter the employee's first name", validate: (fname)=>{
+                return (!/^[a-zA-Z]/i.test(fname))
+            }},
+            {name: "last_name", message: "Enter the employee's last name", validate: (lname)=>{
+                return (!/^[a-zA-Z]/i.test(lname))
+            }},
+            {name: "title", message: "Which role?", type: 'list', choices: roles[0]},
+            {name: "manager", message: "Who is their manager?", type: 'list'},
+            {name: "salary", message: "Enter yearly salary", type: 'number', validate: (salary)=>{
+                return Number.isInteger(salary) || 'Please enter a valid whole number'
+            }},
+            {name: "department", message: "Which department?", type: 'list', choices: departments[0]}]).then((obj)=>{
+                console.log(obj.department)
+                dbconn.query(
+                    `SELECT id FROM employee_db.department WHERE name = ?`, obj.department
+                ).then(result=>{
+                    department_id = result[0][0].id
+                    insertRole(obj.title, obj.salary, department_id)
+                })
+                // console.log(obj)
+        })
+    }
+
     await inq.prompt([{name: "task", message: "What would you like to do?", type: "list", choices: [
         "View All Departments", "View All Roles", "View All Employees", "Add A Department",
         "Add A Role", "Add An Employee", "Update Employee Role",
